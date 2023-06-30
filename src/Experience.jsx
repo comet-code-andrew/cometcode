3e2c3efd8a8616b8417df71ae3ae38aea7edf892
Andrew Heyer <andrewbheyer@gmail.com>
2023-06-30 16:59:42 -0400

    draft of a 3d laptop started, will replace with tablet

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,38 @@
-import {OrbitControls} from "@react-three/drei";
+import {Text, ContactShadows, PresentationControls, Float, Environment, OrbitControls} from "@react-three/drei";
+import Laptop from "./Laptop";
 
 export default function Experience() {
 
   return <>
 
+    <Environment preset="city"/>
+    <color args={['#241a1a']} attach="background"/>
 
+    <PresentationControls
+      global
+      rotation={ [ 0.13, 0.1, 0 ] }
+      polar={ [ - 0.4, 0.2 ] }
+      azimuth={ [ - 1, 0.75 ] }
 
-    <OrbitControls makeDefault />
+    >
+      <Float>
+        <rectAreaLight
+          width={ 2.5 }
+          height={ 1.65 }
+          intensity={ 65 }
+          color={ '#ff6900' }
+          rotation={ [ - 0.1, Math.PI, 0 ] }
+          position={ [ 0, 0.55, - 1.15 ] }
+        />
+        <Laptop position-y={-1.2}/>
+      </Float>
+    </PresentationControls>
 
-    <mesh>
-      <boxGeometry/>
-      <meshNormalMaterial/>
-    </mesh>
-
+    <ContactShadows
+      position-y={-1.4}
+      opacity={0.4}
+      scale={5}
+      blue={2.4}
+    />
   </>
 }
